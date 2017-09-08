3bd792eb74aba587ced35eeb3f2326e2c5ba0e7c
xitongsys <xitongsys@gmail.com>
2017-09-08 23:02:33 +0000

    ..

--- src/parquet_go/ParquetType.go
+++ src/parquet_go/ParquetType.go
@@ -9,4 +9,12 @@ type DOUBLE float64
 type BYTE_ARRAY string
 type FIXED_LEN_BYTE_ARRAY string
 
+type INT_8 byte
+type INT_16 int32
+type INT_32 int32
+type INT_64 int64
 
+type UINT_8 byte
+type UINT_16 uint32
+type UINT_32 uint32
+type UINT_64 uint64
