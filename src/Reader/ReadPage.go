e631f2dc96cef057b0f5eac150b7d1dd36e1e015
xitongsys <xitongsys@gmail.com>
2017-09-14 01:35:17 +0000

    ..

--- src/Reader/ReadPage.go
+++ src/Reader/ReadPage.go
@@ -50,7 +50,7 @@ func ReadPage(thriftReader *thrift.TBufferedTransport, colMetaData *parquet.Colu
 		} else {
 			repetitionLevels = make([]Interface, pageHeader.DataPageHeader.GetNumValues())
 			for i := 0; i < len(repetitionLevels); i++ {
-				repetitionLevels[i] = int64(0)
+				repetitionLevels[i] = int32(0)
 			}
 		}
 
@@ -61,12 +61,36 @@ func ReadPage(thriftReader *thrift.TBufferedTransport, colMetaData *parquet.Colu
 		} else {
 			definitionLevels = make([]interface{}, pageHeader.DataPageHeader.GetNumValues())
 			for i := 0; i < len(definitionLevels); i++ {
-				definitionLevels[i] = int64(0)
+				definitionLevels[i] = int32(0)
 			}
 		}
 
 		var values []interface{}
 		values = ReadValues()
+
+		table := new(Table)
+		table.Path = path
+		name := strings.Join(path, ".")
+		table.Repetition_Type = schemaHandler.SchemaElements[name].GetRepetitionType()
+		table.MaxRepetitionLevel = maxRepetitionLevel
+		table.MaxDefinitionLevel = maxDefinitionLevel
+		table.Values = make([]interface{}, len(definitionLevels))
+		table.RepetitionLevels = make([]int32, len(definitionLevels))
+		table.DefinitionLevels = make([]int32, len(definitionLevels))
+
+		j := 0
+		for i := 0; i < len(definitionLevels); i++ {
+			dl, _ := definitionLevels[i].(int32)
+			rl, _ := repetitionLevels[i].(int32)
+			res.RepetitionLevels[i] = rl
+			res.DefinitionLevels[i] = dl
+			if res.DefinitionLevels[i] == maxDefinitionLevel {
+				res.Values[i] = values[j]
+				j++
+			}
+		}
+
+		page.DataTable = table
 
 	} else if pageHeader.GetType() == parquet.PageType_DICTIONARY_PAGE {
 		page = NewDictPage()
