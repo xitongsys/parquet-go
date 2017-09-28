472fc964d4d0d5342f15fc8abc905412f74d96e3
xitongsys <xitongsys@gmail.com>
2017-09-28 01:01:17 +0000

    adjust example

--- example/write_flat.go
+++ example/write_flat.go
@@ -43,12 +43,12 @@ func nextName(nameStr string) string {
 }
 
 func CreateStudents() []Student {
-	stus := make([]Student, 1000000)
+	stus := make([]Student, 10)
 	stuName := "aaaaa_STU"
 	var id int64 = 1
 	for i := 0; i < len(stus); i++ {
 		stus[i].Name = UTF8(stuName)
-		stus[i].Age = INT32(-i)
+		stus[i].Age = INT32(i)
 		stus[i].Id = INT64(i)
 		stus[i].Weight = FLOAT(50.0 + float32(stus[i].Age)*0.1)
 		stus[i].Sex = BOOLEAN(i%2 == 0)
