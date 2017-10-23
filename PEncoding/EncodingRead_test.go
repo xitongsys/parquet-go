f58b4919c6b6046e54a59a397f30937e9a445d89
xitongsys <xitongsys@gmail.com>
2017-10-23 09:12:36 +0800

    adding ut of pencoding read

---
+++ PEncoding/EncodingRead_test.go
@@ -0,0 +1,26 @@
+package PEncoding
+
+import (
+	"bytes"
+	"fmt"
+	. "github.com/xitongsys/parquet-go/ParquetType"
+	"testing"
+)
+
+func TestReadPlainINT32(t *testing.T) {
+	testData := []struct {
+		expected   []interface{}
+		byteReader *bytes.Reader
+	}{
+		{[]interface{}{}, bytes.NewReader([]byte{})},
+		{[]interface{}{INT32(0)}, bytes.NewReader([]byte{0, 0, 0, 0})},
+		{[]interface{}{INT32(0), INT32(1), INT32(2)}, bytes.NewReader([]byte{0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0})},
+	}
+
+	for _, data := range testData {
+		res := ReadPlainINT32(data.byteReader, uint64(len(data.expected)))
+		if fmt.Sprintf("%v", res) != fmt.Sprintf("%v", data.expected) {
+			t.Errorf("ReadPlainINT32 error, expect %v, get %v", data.expected, res)
+		}
+	}
+}
