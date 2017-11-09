f2c73be869d0f49a11e53ac9dbbbe60699fbdbac
xitongsys <xitongsys@gmail.com>
2017-11-09 11:01:25 +0000

    add parquettype ut

--- ParquetType/ParquetType_test.go
+++ ParquetType/ParquetType_test.go
@@ -112,6 +112,22 @@ func TestStrToParquetType(t *testing.T) {
 		{"0.1", "DOUBLE", DOUBLE(0.1)},
 		{"abc bcd", "BYTE_ARRAY", BYTE_ARRAY("abc bcd")},
 		{"abc bcd", "FIXED_LEN_BYTE_ARRAY", FIXED_LEN_BYTE_ARRAY("abc bcd")},
+		{"abc bcd", "UTF8", UTF8("abc bcd")},
+		{"1", "INT_8", INT_8(1)},
+		{"1", "INT_16", INT_16(1)},
+		{"1", "INT_32", INT_32(1)},
+		{"1", "INT_64", INT_64(1)},
+		{"1", "UINT_8", UINT_8(1)},
+		{"1", "UINT_16", UINT_16(1)},
+		{"1", "UINT_32", UINT_32(1)},
+		{"1", "UINT_64", UINT_64(1)},
+		{"1", "DATE", DATE(1)},
+		{"1", "TIME_MILLIS", TIME_MILLIS(1)},
+		{"1", "TIME_MICROS", TIME_MICROS(1)},
+		{"1", "TIMESTAMP_MICROS", TIMESTAMP_MICROS(1)},
+		{"1", "TIMESTAMP_MILLIS", TIMESTAMP_MILLIS(1)},
+		{"012345678901", "INTERVAL", INTERVAL("012345678901")},
+		{"012345678901", "DECIMAL", DECIMAL("012345678901")},
 	}
 
 	for _, data := range testData {
