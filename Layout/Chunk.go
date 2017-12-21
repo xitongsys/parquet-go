99d3cfefa0db436f3c4f9eae4c54cc14ea7bb964
xitongsys <xitongsys@gmail.com>
2017-12-21 18:03:26 +0800

    adding dict

--- Layout/Chunk.go
+++ Layout/Chunk.go
@@ -73,9 +73,48 @@ func PagesToChunk(pages []*Page) *Chunk {
 //Convert several pages to one chunk with dict page first
 func PagesToChunkWithDictHead(pages []*Page) *Chunk {
 	ln := len(pages)
-	if ln < 2 {
+	if ln <= 0 {
 		return nil
 	}
+
+	table := NewTableFromTable(pages[0].DataTable)
+	table.Type = parquet.Type_INT32
+
+	dictMap := make(map[interface{}]ParquetType.INT32)
+	index := -1
+	dictMap[nil] = -1
+	dictTable := NewTableFromTable(pages[0].DataTable)
+	pageSize := pages[0].PageSize
+
+	for _, page := range pages {
+		ln := len(page.DataTable.Values)
+		for i := 0; i < ln; i++ {
+			val := page.DataTable.Values[i]
+			rl, dl := page.DataTable.RepetitionLevels[i], page.DataTable.DefinitionLevels[i]
+
+			if _, ok := dictMap[val]; !ok {
+				index++
+				dictMap[val] = ParquetType.INT32(index)
+				dictTable.Values = append(dictTable.Values, val)
+			}
+			table.Values = append(table.Values, dictMap[val])
+			table.DefinitionLevels = append(table.DefinitionLevels, dl)
+			table.RepetitionLevels = append(table.RepetitionLevels, rl)
+			if rl > table.MaxRepetitionLevel {
+				table.MaxRepetitionLevel = rl
+			}
+			if dl > table.MaxDefinitionLevel {
+				table.MaxDefinitionLevel = dl
+			}
+		}
+	}
+
+	bitWidth := Common.BitNum(uint64(index))
+	dictPage, _ := TableToDictPage(dictTable, pageSize, parquet.CompressionCodec_SNAPPY)
+	dataPages, _ := TableToDictDataPages(table, pageSize, int32(bitWidth), parquet.CompressionCodec_SNAPPY)
+	dictPages := []*Page{dictPage}
+	dictPages = append(dictPages, dataPages...)
+
 	var numValues int64 = 0
 	var totalUncompressedSize int64 = 0
 	var totalCompressedSize int64 = 0
@@ -99,7 +138,7 @@ func PagesToChunkWithDictHead(pages []*Page) *Chunk {
 	}
 
 	chunk := new(Chunk)
-	chunk.Pages = pages
+	chunk.Pages = dictPages
 	chunk.ChunkHeader = parquet.NewColumnChunk()
 	metaData := parquet.NewColumnMetaData()
 	metaData.Type = pages[1].DataType
@@ -145,54 +184,6 @@ func DecodeDictChunk(chunk *Chunk) {
 		}
 	}
 	chunk.Pages = chunk.Pages[1:] // delete the head dict page
-}
-
-//Encode dict chunk
-func EncodeDictChunk(chunk *Chunk) {
-	if chunk == nil {
-		return
-	}
-	if ln := len(chunk.Pages); ln <= 0 {
-		return
-	}
-	table := NewTableFromTable(chunk.Pages[0].DataTable)
-	table.Type = parquet.Type_INT32
-
-	dictMap := make(map[interface{}]ParquetType.INT32)
-	index := -1
-	dictMap[nil] = -1
-	dictTable := NewTableFromTable(chunk.Pages[0].DataTable)
-	pageSize := chunk.Pages[0].PageSize
-
-	for _, page := range chunk.Pages {
-		ln := len(page.DataTable.Values)
-		for i := 0; i < ln; i++ {
-			val := page.DataTable.Values[i]
-			rl, dl := page.DataTable.RepetitionLevels[i], page.DataTable.DefinitionLevels[i]
-
-			if _, ok := dictMap[val]; !ok {
-				index++
-				dictMap[val] = ParquetType.INT32(index)
-				dictTable.Values = append(dictTable.Values, val)
-			}
-			table.Values = append(table.Values, dictMap[val])
-			table.DefinitionLevels = append(table.DefinitionLevels, dl)
-			table.RepetitionLevels = append(table.RepetitionLevels, rl)
-			if rl > table.MaxRepetitionLevel {
-				table.MaxRepetitionLevel = rl
-			}
-			if dl > table.MaxDefinitionLevel {
-				table.MaxDefinitionLevel = dl
-			}
-		}
-	}
-
-	bitWidth := Common.BitNum(uint64(index))
-	dictPage, _ := TableToDictPage(dictTable, pageSize, parquet.CompressionCodec_SNAPPY)
-	dataPages, _ := TableToDictDataPages(table, pageSize, int32(bitWidth), parquet.CompressionCodec_SNAPPY)
-	pages := []*Page{dictPage}
-	pages = append(pages, dataPages...)
-	chunk = PagesToChunkWithDictHead(pages)
 }
 
 //Read one chunk from parquet file (Deprecated)
