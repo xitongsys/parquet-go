87300c6e891ad33ac38ed5b4042075dae961b645
xitongsys <xitongsys@gmail.com>
2018-12-26 02:07:23 +0000

    add read partial example

---
+++ example/read_partial.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/xitongsys/parquet-go/ParquetFile"
+	"github.com/xitongsys/parquet-go/ParquetReader"
+	"github.com/xitongsys/parquet-go/ParquetWriter"
+	"github.com/xitongsys/parquet-go/parquet"
+)
+
+type Student struct {
+	Name    string  `parquet:"name=name, type=UTF8, encoding=PLAIN_DICTIONARY"`
+	Age     int32   `parquet:"name=age, type=INT32"`
+	Id      int64   `parquet:"name=id, type=INT64"`
+	Weight  float32 `parquet:"name=weight, type=FLOAT"`
+	Sex     bool    `parquet:"name=sex, type=BOOLEAN"`
+	Day     int32   `parquet:"name=day, type=DATE"`
+	Ignored int32   //without parquet tag and won't write
+}
+
+type Student2 struct {
+	Name string `parquet:"name=name, type=UTF8, encoding=PLAIN_DICTIONARY"`
+	Age  int32  `parquet:"name=age, type=INT32"`
+	Id   int64  `parquet:"name=id, type=INT64"`
+	Sex  *bool
+}
+
+func main() {
+	var err error
+	fw, err := ParquetFile.NewLocalFileWriter("flat.parquet")
+	if err != nil {
+		log.Println("Can't create local file", err)
+		return
+	}
+
+	//write
+	pw, err := ParquetWriter.NewParquetWriter(fw, new(Student), 4)
+	if err != nil {
+		log.Println("Can't create parquet writer", err)
+		return
+	}
+
+	pw.RowGroupSize = 128 * 1024 * 1024 //128M
+	pw.CompressionType = parquet.CompressionCodec_SNAPPY
+	num := 100
+	for i := 0; i < num; i++ {
+		stu := Student{
+			Name:   "StudentName",
+			Age:    int32(20 + i%5),
+			Id:     int64(i),
+			Weight: float32(50.0 + float32(i)*0.1),
+			Sex:    bool(i%2 == 0),
+			Day:    int32(time.Now().Unix() / 3600 / 24),
+		}
+		if err = pw.Write(stu); err != nil {
+			log.Println("Write error", err)
+		}
+	}
+	if err = pw.WriteStop(); err != nil {
+		log.Println("WriteStop error", err)
+		return
+	}
+	log.Println("Write Finished")
+	fw.Close()
+
+	///read
+	fr, err := ParquetFile.NewLocalFileReader("flat.parquet")
+	if err != nil {
+		log.Println("Can't open file")
+		return
+	}
+
+	pr, err := ParquetReader.NewParquetReader(fr, new(Student2), 4)
+	if err != nil {
+		log.Println("Can't create parquet reader", err)
+		return
+	}
+	num = int(pr.GetNumRows())
+	for i := 0; i < num/10; i++ {
+		if i%2 == 0 {
+			pr.SkipRows(10) //skip 10 rows
+			continue
+		}
+		stus := make([]Student2, 10) //read 10 rows
+		if err = pr.Read(&stus); err != nil {
+			log.Println("Read error", err)
+		}
+		log.Println(stus)
+	}
+
+	pr.ReadStop()
+	fr.Close()
+
+}
