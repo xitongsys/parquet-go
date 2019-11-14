fa810e5003900468ea667690e1621a1f98755679
Tyrone Groves <tgroves@farmobile.com>
2019-11-13 23:02:26 -0600

    Included build tags to exclude compression methods (and dependencies)

---
+++ compress/gzip.go
@@ -0,0 +1,28 @@
+// +build !no_gzip
+
+package compress
+
+import (
+	"bytes"
+	"compress/gzip"
+	"github.com/tyronegroves/parquet-go/parquet"
+	"io/ioutil"
+)
+
+func init() {
+	compressors[parquet.CompressionCodec_GZIP] = &Compressor{
+		Compress: func(buf []byte) []byte {
+			var res bytes.Buffer
+			gzipWriter := gzip.NewWriter(&res)
+			gzipWriter.Write(buf)
+			gzipWriter.Close()
+			return res.Bytes()
+		},
+		Uncompress: func(buf []byte) (i []byte, err error) {
+			rbuf := bytes.NewReader(buf)
+			gzipReader, _ := gzip.NewReader(rbuf)
+			res, err := ioutil.ReadAll(gzipReader)
+			return res, err
+		},
+	}
+}
