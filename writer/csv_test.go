51d8869dd0fa73705fa151a6e4bdd79782a70345
Alex Gaynor <alex.gaynor@gmail.com>
2020-05-10 12:23:06 -0400

    Added a benchmark of CSV writer for ease of running

---
+++ writer/csv_test.go
@@ -0,0 +1,38 @@
+package writer
+
+import (
+	"testing"
+
+	"github.com/xitongsys/parquet-go-source/buffer"
+)
+
+func BenchmarkWriteCSV(b *testing.B) {
+	b.ReportAllocs()
+	md := []string{
+		"name=First, type=UTF8, encoding=PLAIN",
+		"name=Middle, type=UTF8, encoding=PLAIN",
+		"name=Last, type=UTF8, encoding=PLAIN",
+		"name=BirthCity, type=UTF8, encoding=PLAIN",
+	}
+	for i := 0; i < b.N; i++ {
+		fw, err := buffer.NewBufferFile(nil)
+		if err != nil {
+			b.Fatal(err)
+		}
+		pw, err := NewCSVWriter(md, fw, 2)
+		if err != nil {
+			b.Fatal(err)
+		}
+		for j := 0; j < 10000; j++ {
+			err = pw.Write([]interface{}{"Harry", "S", "Truman", "Lamar"})
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+		err = pw.WriteStop()
+		if err != nil {
+			b.Fatal(err)
+		}
+		fw.Close()
+	}
+}
