9eae05a75baddc9fb11ce13359a9c666855177ca
Alex Gaynor <alex.gaynor@gmail.com>
2020-05-21 21:07:27 -0400

    Added a benchmark using dictionary encoding

--- writer/csv_test.go
+++ writer/csv_test.go
@@ -36,3 +36,34 @@ func BenchmarkWriteCSV(b *testing.B) {
 		fw.Close()
 	}
 }
+
+func BenchmarkWriteCSVPlainDictionary(b *testing.B) {
+	b.ReportAllocs()
+	md := []string{
+		"name=First, type=UTF8, encoding=PLAIN_DICTIONARY",
+		"name=Middle, type=UTF8, encoding=PLAIN_DICTIONARY",
+		"name=Last, type=UTF8, encoding=PLAIN_DICTIONARY",
+		"name=BirthCity, type=UTF8, encoding=PLAIN_DICTIONARY",
+	}
+	for i := 0; i < b.N; i++ {
+		fw, err := buffer.NewBufferFile(nil)
+		if err != nil {
+			b.Fatal(err)
+		}
+		pw, err := NewCSVWriter(md, fw, 2)
+		if err != nil {
+			b.Fatal(err)
+		}
+		for j := 0; j < 10000; j++ {
+			err = pw.Write([]interface{}{"Harry", "S", "Truman", "Lamar"})
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+		err = pw.WriteStop()
+		if err != nil {
+			b.Fatal(err)
+		}
+		fw.Close()
+	}
+}
