9852c5527a61178384cf35ad167fe645e2715085
Ryo Okubo <syucream1031@gmail.com>
2020-08-10 16:21:28 +0900

    Stop using parquet-go-source in writer for now

--- writer/json.go
+++ writer/json.go
@@ -1,11 +1,12 @@
 package writer
 
 import (
+	"io"
+
 	"github.com/syucream/parquet-go/layout"
 	"github.com/syucream/parquet-go/marshal"
 	"github.com/syucream/parquet-go/parquet"
 	"github.com/syucream/parquet-go/schema"
-	"github.com/xitongsys/parquet-go/source"
 )
 
 type JSONWriter struct {
@@ -13,7 +14,7 @@ type JSONWriter struct {
 }
 
 //Create JSON writer
-func NewJSONWriter(jsonSchema string, pfile source.ParquetFile, np int64) (*JSONWriter, error) {
+func NewJSONWriter(jsonSchema string, w io.WriteCloser, np int64) (*JSONWriter, error) {
 	var err error
 	res := new(JSONWriter)
 	res.SchemaHandler, err = schema.NewSchemaHandlerFromJSON(jsonSchema)
@@ -21,7 +22,7 @@ func NewJSONWriter(jsonSchema string, pfile source.ParquetFile, np int64) (*JSON
 		return res, err
 	}
 
-	res.PFile = pfile
+	res.w = w
 	res.PageSize = 8 * 1024              //8K
 	res.RowGroupSize = 128 * 1024 * 1024 //128M
 	res.CompressionType = parquet.CompressionCodec_SNAPPY
@@ -32,7 +33,7 @@ func NewJSONWriter(jsonSchema string, pfile source.ParquetFile, np int64) (*JSON
 	res.Footer.Version = 1
 	res.Footer.Schema = append(res.Footer.Schema, res.SchemaHandler.SchemaElements...)
 	res.Offset = 4
-	_, err = res.PFile.Write([]byte("PAR1"))
+	_, err = res.w.Write(magic)
 	res.MarshalFunc = marshal.MarshalJSON
 	return res, err
 }
