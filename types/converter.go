fe900cf2a78491298b099cd4a9d9c0ede7c7178f
xitongsys <xitongsys@gmail.com>
2020-12-01 14:39:53 +0800

    add convert func

--- types/converter.go
+++ types/converter.go
@@ -2,25 +2,44 @@ package types
 
 import (
 	"time"
+	"encoding/binary"
 )
 
-func Time2Millis(t time.Time, adjustedToUTC bool) int64 {
-	return Time2Micros(t, adjustedToUTC) / 1000
+func TimeToTIME_MILLIS(t time.Time, adjustedToUTC bool) int64 {
+	return TimeToTIME_MICROS(t, adjustedToUTC) / 1000
 }
 
-func Millis2Time(millis int64, adjustedToUTC bool) time.Time {
-	return Micros2Time(millis * 1000, adjustedToUTC)
+func TimeToTIME_MICROS(t time.Time, adjustedToUTC bool) int64 {
+	if adjustedToUTC {
+		tu := t.UTC()
+		h, m, s, ns := int64(tu.Hour()), int64(tu.Minute()), int64(tu.Second()), int64(tu.Nanosecond())
+		nanos := h * int64(time.Hour) + m * int64(time.Minute) + s * int64(time.Second) + ns * int64(time.Nanosecond)
+		return nanos / 1000
+
+	} else {
+		h, m, s, ns := int64(t.Hour()), int64(t.Minute()), int64(t.Second()), int64(t.Nanosecond())
+		nanos := h * int64(time.Hour) + m * int64(time.Minute) + s * int64(time.Second) + ns * int64(time.Nanosecond)
+		return nanos / 1000
+	}
+}
+
+func TimeToTIMESTAMP_MILLIS(t time.Time, adjustedToUTC bool) int64 {
+	return TimeToTIMESTAMP_MICROS(t, adjustedToUTC) / 1000
+}
+
+func TIMESTAMP_MILLISToTime(millis int64, adjustedToUTC bool) time.Time {
+	return TIMESTAMP_MICROSToTime(millis * 1000, adjustedToUTC)
 } 
 
-func Time2Micros(t time.Time, adjustedToUTC bool) int64 {
-	return Time2Nanos(t, adjustedToUTC) / 1000
+func TimeToTIMESTAMP_MICROS(t time.Time, adjustedToUTC bool) int64 {
+	return TimeToTIMESTAMP_NANOS(t, adjustedToUTC) / 1000
 }
 
-func Micros2Time(micros int64, adjustedToUTC bool) time.Time {
-	return Nanos2Time(micros * 1000, adjustedToUTC)
+func TIMESTAMP_MICROSToTime(micros int64, adjustedToUTC bool) time.Time {
+	return TIMESTAMP_NANOSToTime(micros * 1000, adjustedToUTC)
 }
 
-func Time2Nanos(t time.Time, adjustedToUTC bool) int64 {
+func TimeToTIMESTAMP_NANOS(t time.Time, adjustedToUTC bool) int64 {
 	if adjustedToUTC {
 		return t.UnixNano()
 	} else {
@@ -29,7 +48,7 @@ func Time2Nanos(t time.Time, adjustedToUTC bool) int64 {
 	}
 }
 
-func Nanos2Time(nanos int64, adjustedToUTC bool) time.Time {
+func TIMESTAMP_NANOSToTime(nanos int64, adjustedToUTC bool) time.Time {
 	if adjustedToUTC {
 		return time.Unix(0, nanos).UTC()
 
@@ -40,10 +59,25 @@ func Nanos2Time(nanos int64, adjustedToUTC bool) time.Time {
 	}
 }
 
-func Time2INT96(t time.Time) string {
-
+func TimeToINT96(t time.Time) string {
+	return ""
 }
 
-func INT962Time(int96 string) time.Time {
+func INT96ToTime(int96 string) time.Time {
+	nano := binary.LittleEndian.Uint64([]byte(int96[:8]))
+	dt := binary.LittleEndian.Uint32([]byte(int96[8:]))
 
+	l := dt + 68569
+	n := 4 * l / 146097
+	l = l - (146097*n+3)/4
+	i := 4000 * (l + 1) / 1461001
+	l = l - 1461*i/4 + 31
+	j := 80 * l / 2447
+	k := l - 2447*j/80
+	l = j / 11
+	j = j + 2 - 12*l
+	i = 100*(n-49) + i + l
+	tm := time.Date(int(i), time.Month(j), int(k), 0, 0, 0, 0, time.UTC)
+	tm = tm.Add(time.Duration(nano))
+	return tm
 }
