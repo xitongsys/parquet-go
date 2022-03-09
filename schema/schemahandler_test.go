deaf74b21f756eedf4c9cabbc9abb485937a621f
Johannes Würbach <johannes.wuerbach@googlemail.com>
2022-03-09 01:03:32 +0100

    fix schema handler test

--- schema/schemahandler_test.go
+++ schema/schemahandler_test.go
@@ -6,20 +6,23 @@ import (
 )
 
 type Class struct {
-	Name   string `parquet:"name=name, type=UTF8"`
+	Name   string `parquet:"name=name, type=BYTE_ARRAY, convertedtype=UTF8"`
 	Number int    `parquet:"name=number, type=INT32"`
 	Score  string `parquet:"scale=3, precision=10, type=INT32"`
 }
 
 type Student struct {
-	Name    string              `parquet:"name=name, type=UTF8"`
+	Name    string              `parquet:"name=name, type=BYTE_ARRAY, convertedtype=UTF8"`
 	Age     int                 `parquet:"name=number, type=INT64"`
 	Classes []*Class            `parquet:"name=classes"`
-	Info    *map[string]*string `parquet:"name=info, type=UTF8, keytype=UTF8"`
+	Info    *map[string]*string `parquet:"name=info, type=MAP, convertedtype=MAP, keytype=BYTE_ARRAY, keyconvertedtype=UTF8, valuetype=BYTE_ARRAY, valueconvertedtype=UTF8"`
 	Sex     *bool               `parquet:"name=sex, type=BOOLEAN"`
 }
 
 func TestNewSchemaHandlerFromStruct(t *testing.T) {
-	schemaMap, _ := NewSchemaHandlerFromStruct(new(Student))
+	schemaMap, err := NewSchemaHandlerFromStruct(new(Student))
+	if err != nil {
+		t.Errorf("error in creating schema from struct :%v", err.Error())
+	}
 	fmt.Println(schemaMap)
 }
