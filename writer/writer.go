6337ad3893950d541ac2b3f2e6a388ffccf795ac
Robert Borkowski <rb81@o2.pl>
2023-10-09 14:33:40 +0200

    Fix page index, issues/547

--- writer/writer.go
+++ writer/writer.go
@@ -473,7 +473,7 @@ func (pw *ParquetWriter) Flush(flag bool) error {
 					pageLocation := parquet.NewPageLocation()
 					pageLocation.Offset = pw.Offset
 					pageLocation.FirstRowIndex = firstRowIndex
-					pageLocation.CompressedPageSize = page.Header.CompressedPageSize
+					pageLocation.CompressedPageSize = int32(len(page.RawData)) // https://github.com/xitongsys/parquet-go/issues/547
 
 					offsetIndex.PageLocations = append(offsetIndex.PageLocations, pageLocation)
 
