ae692cf50440c766226be08ba219ec0bf4a9ca56
wenruimeng-work <wenrui@Wenruis-Laptop.local>
2024-02-05 19:16:05 -0800

    [Ursa Log Analyzer] Support proto or go struct object to parquet file

---
+++ writer/proto.go
@@ -0,0 +1,23 @@
+package writer
+
+import (
+	"fmt"
+
+	"github.com/xitongsys/parquet-go/marshal"
+	"github.com/xitongsys/parquet-go/schema"
+	"github.com/xitongsys/parquet-go/source"
+)
+
+// Pass the obj as the go struct object
+func NewParquetWriterFromProto(pFile source.ParquetFile, obj interface{}, np int64) (*ParquetWriter, error) {
+	schemaHandler, err := schema.NewSchemaHandlerFromProtoStruct(obj)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate schema handler: %v", err)
+	}
+	parquetWriter, err := NewParquetWriter(pFile, schemaHandler, np)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create writer %v", err)
+	}
+	parquetWriter.MarshalFunc = marshal.MarshalProto
+	return parquetWriter, nil
+}
